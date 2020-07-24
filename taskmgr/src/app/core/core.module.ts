d0c9fa7bf2370123604d9d40e2f176ceff5cb3e4
nienie <765891612@qq.com>
2020-07-24 23:45:05 +0800

    icon 使用

--- taskmgr/src/app/core/core.module.ts
+++ taskmgr/src/app/core/core.module.ts
@@ -1,13 +1,23 @@
 import { NgModule, SkipSelf, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { loadSvgResources } from '../utils/svg.util';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, SidebarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [
     HeaderComponent,
@@ -18,9 +28,14 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 export class CoreModule {
   // 使用SkipSelf()注解，避免重复注入，去系统的父级查找依赖
   // 使用Optional()注解，让SkipSelf作为可选，在第一次系统中并没有CoreModule的时候成功注入
-  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+  constructor(
+    @Optional() @SkipSelf() parent: CoreModule,
+    ir: MatIconRegistry,
+    ds: DomSanitizer
+  ) {
     if (parent) {
       throw new Error('模块已加载，不需要重新加载');
     }
+    loadSvgResources(ir, ds);
   }
 }
--- taskmgr/src/app/core/header/header.component.ts
+++ taskmgr/src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() toggle = new EventEmitter<void>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openSidebar() {
+    // 打开侧边栏，将点击事件发送出去
+    this.toggle.emit();
   }
 
 }
