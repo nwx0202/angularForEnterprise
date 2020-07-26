33afc08de50009c1fa4e61c498e040964de2f152
nienie <765891612@qq.com>
2020-07-26 23:55:25 +0800

    晚上被几个流氓软件弄烦了！明天继续！

--- taskmgr/src/app/core/core.module.ts
+++ taskmgr/src/app/core/core.module.ts
@@ -1,9 +1,6 @@
 import { NgModule, SkipSelf, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconRegistry } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
 
@@ -11,15 +8,14 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { loadSvgResources } from '../utils/svg.util';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, SidebarComponent],
   imports: [
     CommonModule,
     HttpClientModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule
+    SharedModule
   ],
   exports: [
     HeaderComponent,
---
+++ taskmgr/src/app/domain/user.module.ts
@@ -0,0 +1,7 @@
+export interface user {
+  id?: number;
+  email: string;
+  password: string;
+  name: string;
+  avatar: string;
+}
